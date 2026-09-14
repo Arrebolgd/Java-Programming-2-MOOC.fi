4b4d1e6efd3c5706ed4fa2bc4e75c2bb98be2b4a
Arrebolgd <arrebolgd@gmail.com>
2026-09-14 17:45:26 +0200

    Exercise 8_12 completed

--- part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
+++ part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
@@ -51,4 +51,8 @@ public class SimpleDate {
         return this.year - other.year - yearRemoved;
     }
 
+
+    public int hashCode() {
+        return (this.day + "/" + this.month + "/" + this.year).hashCode();
+    }
 }
